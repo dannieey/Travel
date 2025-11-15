d9b41528019b3b03deae8412d5e0d681f6c480d3
Danel <danel.li@mail.ru>
2025-11-16 01:52:43 +0500

    commit

--- src/booking/UserObserver.java
+++ src/booking/UserObserver.java
@@ -1,4 +1,13 @@
 package booking;
 
 public class UserObserver {
+    private String contact; // email или телефон
+
+    public UserObserver(String contact) {
+        this.contact = contact;
+    }
+
+    public void update(double newPrice) {
+        System.out.printf("Notification for %s: the price has decreased to %.2f\n", contact, newPrice);
+    }
 }
--- src/transport/Cruise.java
+++ src/transport/Cruise.java
@@ -1,4 +1,24 @@
 package transport;
 
-public class Cruise {
+public class Cruise implements Transport {
+    private double basePrice;
+
+    public Cruise(double basePrice) {
+
+        this.basePrice = basePrice;
+    }
+
+    @Override
+    public String getName() {
+
+        return "Cruise";
+    }
+
+    @Override
+    public double getBasePrice() {
+
+        return basePrice;
+    }
+
+
 }
